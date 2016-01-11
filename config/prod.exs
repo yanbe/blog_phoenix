ab0c2afc1175965aa483cdb00d33d22e4a663d60
yanbe <y.yanbe@gmail.com>
2016-01-11 11:45:33 +0900

    Revert "change hostname"
    This reverts commit e1e60e898e1b60e9a2e2059d270056a0b4570ac7.

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :blog_phoenix, BlogPhoenix.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "gentle-coast-2422.herokuapp.com", port: 443],
+  url: [host: "pure-plateau-7186.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
   secret_key_base: System.get_env("SECRET_KEY_BASE")
